e69943932ba27bad0c49e3b83bed2095762c9d90
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-03 22:18:26 +0200

    feat: add solution for create and copy tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const folderPath = path.join(import.meta.dirname, 'files');
+    const folderCopyPath = path.join(import.meta.dirname, 'files_copy');
+    
+    try {
+        const folderContent = await fs.readdir(folderPath);
+        await fs.mkdir(folderCopyPath);
+        for (const file of folderContent) {
+            await fs.copyFile(path.join(folderPath, file), path.join(folderCopyPath, file));
+        }
+    }
+    catch {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const folderPath = path.join(import.meta.dirname, 'files');
+    const filePath = path.join(folderPath, 'fresh.txt');
+    const content = 'I am fresh and young';
+    
+    try {
+        await fs.access(filePath);
+        throw new Error ('FS operation failed');
+    }
+    catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(filePath, content);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
