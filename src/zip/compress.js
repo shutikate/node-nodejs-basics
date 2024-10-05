982867d64381b0f48bc23f4561bcbafbd7b7c528
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-05 18:14:42 +0200

    feat: add solution for compress and decompress tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const folderPath = path.join(import.meta.dirname, 'files');
+    const filePath = path.join(folderPath, 'fileToCompress.txt');
+    pipeline (
+        createReadStream(filePath),
+        createGzip(),
+        createWriteStream(path.join(folderPath, 'archive.gz')),
+        (err) => {
+            if (err) {console.error(err)}
+        }
+    )
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const folderPath = path.join(import.meta.dirname, 'files');
+    const filePath = path.join(folderPath, 'archive.gz');
+    pipeline (
+        createReadStream(filePath),
+        createGunzip(),
+        createWriteStream(path.join(folderPath, 'fileToCompress.txt')),
+        (err) => {
+            if (err) {console.error(err)}
+        }
+    )
 };
 
 await decompress();
