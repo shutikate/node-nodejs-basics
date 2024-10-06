e93f8083db993f7e5fcb64489939fdfa2b22c091
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2024-10-06 20:08:42 +0200

    feat: add solution for wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+import path from 'node:path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfHostMachine = os.cpus().length;
+    const filePath = path.join(import.meta.dirname, 'worker.js');
+    const workers = [];
+    for (let i = 0; i < numberOfHostMachine; i++) {
+        const worker = new Worker(filePath, {
+            workerData: i + 10
+        })
+
+        workers.push(new Promise((resolve, reject) => {
+            worker.on('message', (message) => resolve({status: 'resolved', data: message}));
+            worker.on('error', () => reject({status: 'error', data: null}));
+        }))
+    }
+    const result = await Promise.allSettled(workers);
+    console.log(result.map((el) => el.value || el.reason));
 };
 
 await performCalculations();
